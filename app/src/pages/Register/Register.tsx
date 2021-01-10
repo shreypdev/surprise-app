6b1d91bbe1ddf679863d94d4f5ffa2dc7f54fbe9
Siddharth Pathak <siddharthpathak@uvic.ca>
2021-01-10 01:18:37 -0800

    Added Registeration UI v1.0

--- app/src/pages/Register/Register.tsx
+++ app/src/pages/Register/Register.tsx
@@ -1,10 +1,42 @@
-import { IonPage, IonHeader, IonToolbar, IonText, IonContent, IonRow, IonCol } from '@ionic/react';
-import React from 'react';
+import { IonPage, IonHeader, IonToolbar, IonText,IonRouterLink, IonContent, IonRow, IonCol } from '@ionic/react';
+import React, { useState } from 'react';
 import Login from '../Login/Login';
 
+import {InputWithStackedLabel} from '../../components/Inputs/Inputs';
+import {RoundSolidButton} from '../../components/Buttons/Buttons';
+
 import '../Login/Login.scss';
+import { useLoader } from '../../controllers/LoaderManager/LoaderManager';
+import { useToast } from '../../controllers/ToastManager/ToastManager';
+import {SignupModel} from './model';
+
+const initialState = {
+    name: undefined,
+    email: undefined,
+    password: undefined
+};
 
 const Register: React.FC = () => {
+    const [SignupFields, setSignupFields] = useState(initialState);
+
+    const Toast = useToast();
+    const Loader = useLoader();
+
+    const onInputChange = (val: any, key: keyof SignupModel) =>{
+        SignupFields[key] = val;
+        setSignupFields({...SignupFields});
+    };
+
+    //Async function to handle empty user inputs on resgisteration form
+    const handleSignup = async() =>{
+        const{email, name, password} = SignupFields;
+        console.log(SignupFields);
+
+        if(!email || !name || !password){
+            Toast.error("Field cannot be empty!");
+            return
+        }
+    }
 
     return (
         <IonPage>
@@ -22,6 +54,43 @@ const Register: React.FC = () => {
                             <IonText className="ion-text-left" color="primary">
                                 <h2 className="title-bold">Register</h2>
                             </IonText>
+                            <div className="signup-form-inputs">
+                            <InputWithStackedLabel 
+                                    label = {"Email"}
+                                    type="email"
+                                    placeholder="email@gmail.com"
+                                    clearInput
+                                    clearOnEdit
+                                    required
+                                    className="form-input-item"
+                                    onIonChange={(e: any) => onInputChange(e.target.value, 'email')}
+                                />
+                                <InputWithStackedLabel 
+                                    label = {"Password"}
+                                    type="password"
+                                    placeholder="*******"
+                                    clearInput
+                                    clearOnEdit
+                                    required
+                                    className="form-input-item"
+                                    onIonChange={(e: any) => onInputChange(e.target.value, 'password')}
+                                />
+
+                                <RoundSolidButton expand="block" onClick={handleSignup}>
+                                    Register
+                                </RoundSolidButton>
+                                <div
+                                    className="ion-text-center"
+                                    style={{ marginTop: '30px' }}
+                                >
+                                    <IonRouterLink
+                                    href="/login"
+                                    routerDirection="forward"
+                                    >
+                                    Back to login
+                                    </IonRouterLink>
+                                </div>
+                            </div>
                         </form>
                     </IonCol>
                 </IonRow>
